b77048deb6f793ea52c0e028fce2326546d06a7e
ParadoxInfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2021-09-20 23:04:28 +0530

    feat: added mongodb, untested

--- go-server/exchange.go
+++ go-server/exchange.go
@@ -8,19 +8,12 @@ import (
 
 // Stock structure
 type stock struct {
+	MongoID      string  `json:"_id"` // Mongo ID
 	ID       string  `json:"id"`
 	Name     string  `json:"name"`
 	Symbol   string  `json:"symbol"`
 	Price    float64 `json:"price"`
 	Exchange string  `json:"exchange"` // TODO: OPTIONAL: add multiple exchanges
-}
-
-// TODO: This is v0.0.1 sample data. Will have to migrate this to DB in future commits.
-var stocks = []stock{
-	{ID: "stonkschange_00000001", Name: "C", Symbol: "C", Price: 3.14, Exchange: "STNKS"},
-	{ID: "stonkschange_00000010", Name: "C++", Symbol: "CPP", Price: 420.00, Exchange: "STNKS"},
-	{ID: "stonkschange_00000011", Name: "JavaScript", Symbol: "JS", Price: 69.69, Exchange: "STNKS"},
-	{ID: "stonkschange_00000100", Name: "TypeScript", Symbol: "TS", Price: 9001.00, Exchange: "STNKS"},
 }
 
 type Exchange struct {
